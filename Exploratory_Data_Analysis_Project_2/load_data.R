34f88d92dbf38796bde5d5e0455a6ec3df491f20
ChrisYangS <chrisyoung2011@foxmail.com>
2024-01-16 01:27:02 +1300

    initialise Exploratory_Data_Analysis_Project_2

---
+++ Exploratory_Data_Analysis_Project_2/load_data.R
@@ -0,0 +1,23 @@
+# PART ONE: download data from 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip' to ./data folder and unzip it into the same folder
+
+# check if the download folder exists, if not, create it
+if (!file.exists("./data")) {
+    dir.create("./data")
+    print("Creating ./data folder")
+}
+# then check if the zip file exists, if not, download it
+if (!file.exists("./data/NEI_data.zip")) {
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = "./data/NEI_data.zip")
+    print("Downloaded NEI_data.zip")
+    unzip("./data/NEI_data.zip", exdir = "./data")
+    print("Unzipped NEI_data.zip")
+}
+
+# STEP TWO: Read unziped data into R
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
+
+
+# STEP THREE: Answer the following questions
+
+# Question 1: Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
